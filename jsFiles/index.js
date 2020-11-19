27a48e65ff94b311b0165ac3e3ffdb5aadf8bd8b
Boldhonor <ssunil@ch.iitr.ac.in>
2020-11-19 21:15:33 +0530

    Update index.js

--- jsFiles/index.js
+++ jsFiles/index.js
@@ -517,7 +517,8 @@ phoneback.addEventListener('mouseup',function(){phoneSpeed = 0;});
       var t = new THREE.Vector2(0,0);
       renderer.getSize ( t );
 
-      console.log('width '+t.x +'height '+t.y + 'device pix '+ renderer.getPixelRatio () );   
+      console.log('width '+t.x +'height '+t.y + 'device pix '+ renderer.getPixelRatio () );  
+      console.log('h2 '+document.body.clientHeight +'height '+Window.innerHeight );
      // console.log('2 width '+window.screen.availHeight+'height '+window.screen.availHeight );
     }
     else{
